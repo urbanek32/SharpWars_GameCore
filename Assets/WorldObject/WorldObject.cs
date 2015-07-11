8358482843971d5bb55d0241a86f945298ff2bdf
Patryk <urbanek32@gmail.com>
2015-07-11 18:50:50 +0200

    PG-59: podwaliny pod obiekty w świecie. Delikatna poprawa struktury folderów

---
+++ Assets/WorldObject/WorldObject.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldObject : MonoBehaviour {
+
+	public string objectName;
+	public Texture2D buildImage;
+	public int cost, sellValue, hitPoints, maxHitPoints;
+
+	protected Player player;
+	protected string[] actions = {};
+	protected bool currentlySelected = false;
+
+
+
+	protected virtual void Awake()
+	{
+
+	}
+
+	// Use this for initialization
+	protected virtual void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	protected virtual void Update () 
+	{
+	
+	}
+
+	protected virtual void OnGUI()
+	{
+
+	}
+
+
+
+	public void SetSelection(bool selected)
+	{
+		currentlySelected = selected;
+	}
+
+	public string[] GetActions()
+	{
+		return actions;
+	}
+
+	public virtual void PreformAction(string actionToPreform)
+	{
+		//it is up to children with specific actions to determine what to do with each of those actions
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
