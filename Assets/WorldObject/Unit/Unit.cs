1727c246cb31cb52859600ae8028a7e4e2f6ee99
Patryk <urbanek32@gmail.com>
2015-07-14 18:19:45 +0200

    PG-79: obsługujemy już chęć poruszenia jednostką

--- Assets/WorldObject/Unit/Unit.cs
+++ Assets/WorldObject/Unit/Unit.cs
@@ -5,6 +5,12 @@ using RTS;
 
 public class Unit : WorldObject {
 
+	protected bool moving, rotating;
+
+	private Vector3 destination;
+	private Quaternion targetRotation;
+
+
 	protected override void Awake() 
 	{
 		base.Awake();
@@ -26,6 +32,34 @@ public class Unit : WorldObject {
 	}
 
 
+	 
+
+
+	public void StartMove(Vector3 destination)
+	{
+		this.destination = destination;
+		targetRotation = Quaternion.LookRotation(destination - transform.position);
+		rotating = true;
+		moving = false;
+	}
+
+	public override void MouseClick(GameObject hitObject, Vector3 hitPoint, Player controller)
+	{
+		base.MouseClick(hitObject, hitPoint, controller);
+
+		//only handle input if owned by a human player and currently selected
+		if(player && player.human && currentlySelected) 
+		{
+			if(hitObject.name == "Ground" && hitPoint != ResourceManager.InvalidPosition) {
+				float x = hitPoint.x;
+				//makes sure that the unit stays on top of the surface it is on
+				float y = hitPoint.y + player.SelectedObject.transform.position.y;
+				float z = hitPoint.z;
+				Vector3 destination = new Vector3(x, y, z);
+				StartMove(destination);
+			}
+		}
+	}
 
 	public override void SetHoverState(GameObject hoverObject)
 	{
