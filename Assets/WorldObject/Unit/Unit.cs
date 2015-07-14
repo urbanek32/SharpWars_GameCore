80f4cc176640dacdb16e032c9543f2296bc66af5
Patryk <urbanek32@gmail.com>
2015-07-14 12:44:30 +0200

    PG-72: prototyp czołgu jako jednostki

---
+++ Assets/WorldObject/Unit/Unit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : WorldObject {
+
+	protected override void Awake() 
+	{
+		base.Awake();
+	}
+	
+	protected override void Start () 
+	{
+		base.Start();
+	}
+	
+	protected override void Update () 
+	{
+		base.Update();
+	}
+	
+	protected override void OnGUI() 
+	{
+		base.OnGUI();
+	}
+
+}
