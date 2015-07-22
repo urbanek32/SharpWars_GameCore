8f32196587ab8feb9e7ff1e4cbca651c1b6df6d6
Patryk <urbanek32@gmail.com>
2015-07-22 14:12:42 +0200

    PG-109: Zasody dodane do klasy Player

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+using RTS;
 
 public class Player : MonoBehaviour {
 
@@ -7,6 +10,18 @@ public class Player : MonoBehaviour {
 	public bool human;
 	public HUD hud;
 	public WorldObject SelectedObject { get; set; }
+	public int startMoney, startMoneyLimit, startPower, startPowerLimit;
+
+	private Dictionary< ResourceType, int > resources, resourceLimits;
+
+	// works like constructor
+	void Awake() 
+	{
+		resources = InitResourceList();
+		resourceLimits = InitResourceList();
+		AddStartResourceLimits();
+		AddStartResources();
+	}
 
 	// Use this for initialization
 	void Start () 
@@ -20,6 +35,39 @@ public class Player : MonoBehaviour {
 	
 	}
 
+
+
+	public void AddResource(ResourceType type, int amount) 
+	{
+		resources[type] += amount;
+	}
+	
+	public void IncrementResourceLimit(ResourceType type, int amount) 
+	{
+		resourceLimits[type] += amount;
+	}
+
+
+
+	private Dictionary< ResourceType, int > InitResourceList()
+	{
+		Dictionary< ResourceType, int > list = new Dictionary< ResourceType, int >();
+		list.Add(ResourceType.Money, 0);
+		list.Add(ResourceType.Power, 0);
+		return list;
+	}
+
+	private void AddStartResourceLimits()
+	{
+		IncrementResourceLimit(ResourceType.Money, startMoneyLimit);
+		IncrementResourceLimit(ResourceType.Power, startPowerLimit);
+	}
+
+	private void AddStartResources() 
+	{
+		AddResource(ResourceType.Money, startMoney);
+		AddResource(ResourceType.Power, startPower);
+	}
 
 
 
--- Assets/RTS/Enums.cs
+++ Assets/RTS/Enums.cs
@@ -6,6 +6,6 @@ using RTS;
 namespace RTS
 {
 	public enum CursorState { Select, Move, Attack, PanLeft, PanRight, PanUp, PanDown, Harvest }
-
+	public enum ResourceType { Money, Power }
 
 }
