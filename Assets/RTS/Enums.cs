7cbc286a638c201122976ccb2ccc1bbf9854bdca
Patryk <urbanek32@gmail.com>
2015-07-28 21:58:21 +0200

    PG-132: Ładny prefab złoża rudy i skrypt od niego plus materiał. Plus skrypt od obsługi protokołu

--- Assets/RTS/Enums.cs
+++ Assets/RTS/Enums.cs
@@ -6,6 +6,6 @@ using RTS;
 namespace RTS
 {
 	public enum CursorState { Select, Move, Attack, PanLeft, PanRight, PanUp, PanDown, Harvest, RallyPoint }
-	public enum ResourceType { Money, Power }
+	public enum ResourceType { Money, Power, Ore, Unknown }
 
 }
---
+++ Assets/WorldObject/Resource/OreDeposit/Ore.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+using RTS;
+
+public class Ore : MonoBehaviour 
+{
+	//wrapper class for getting access to a part of an ore deposit
+}
