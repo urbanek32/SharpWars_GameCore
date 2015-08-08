e3a902f9da86179f2033ac2dec745ff9de0b15a8
Patryk <urbanek32@gmail.com>
2015-08-08 17:51:21 +0200

    cos tam mp

---
+++ Assets/TestStuff/Player_NetworkSetup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour {
+
+	[SerializeField] Camera FPSCharacterCam;
+	[SerializeField] AudioListener audioListener;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(isLocalPlayer)
+		{
+			GameObject.Find("Scene Camera").SetActive(false);
+			GetComponent<CharacterController>().enabled = true;
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+			FPSCharacterCam.enabled = true;
+			audioListener.enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
