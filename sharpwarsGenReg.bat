16cc004a23bb41dd12bbb78d729929e1e060ea7a
Adrian Pielech <apielech@hotmail.com>
2015-11-06 14:24:28 +0100

    Generator instalatora protokołu[Batch].

---
+++ sharpwarsGenReg.bat
@@ -0,0 +1,13 @@
+echo Windows Registry Editor Version 5.00 >> InstalujProtokol.reg
+echo.  >> InstalujProtokol.reg
+echo [HKEY_CLASSES_ROOT\sharpwars] >> InstalujProtokol.reg
+echo @="URL: SharpWars Game Protocol 4 Uber Pro Hax" >> InstalujProtokol.reg
+echo "URL Protocol"="" >> InstalujProtokol.reg
+echo.  >> InstalujProtokol.reg
+echo [HKEY_CLASSES_ROOT\sharpwars\shell] >> InstalujProtokol.reg
+echo.  >> InstalujProtokol.reg
+echo [HKEY_CLASSES_ROOT\sharpwars\shell\open] >> InstalujProtokol.reg
+echo.  >> InstalujProtokol.reg
+echo [HKEY_CLASSES_ROOT\sharpwars\shell\open\command] >> InstalujProtokol.reg
+echo @="\"%CD:\=\\%\\SharpWars.exe\" \"%1\"" >> InstalujProtokol.reg
+echo.  >> InstalujProtokol.reg
